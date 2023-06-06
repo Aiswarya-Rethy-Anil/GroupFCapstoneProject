a310df99cf6a8d63a91395c2ea455bc50892e046
Philip Augustine Naijan <65936214+Flip1997@users.noreply.github.com>
2023-06-06 18:48:00 -0400

    Delete app directory

--- app/src/main/java/com/example/expensetracker/config.java
+++
@@ -1,7 +0,0 @@
-package com.example.expensetracker;
-
-public interface config {
-
-    String baseurl="http://10.0.0.147/ExpenseTrackerphp/";
-    String imgurl="http://10.0.0.147/ExpenseTrackerphp/uploads/";
-}
